b8f1fe54b111564df0bae70cd9e0d3a13b1f91f7
vicentemarvin-dotcom <vicente.marvin@student.ccm.edu>
2026-09-09 10:58:18 -0400

    Complete Week 1 Lab 1

---
+++ character_profile.py
@@ -0,0 +1,12 @@
+name= "Zussman"
+age=22
+quote="That's whats happens when you try to huslte a hustler"
+quote2="3 facts about this character"
+quote3="Robert Zussman is the deuteragonist and a primary squad member in the 2017 video game Call of Duty WWII."
+quote4="He is the best friend and close combat companison of the game's protagonist, private Ronald_Red_Daniels."
+print(name)
+print(age)
+print(quote)
+print(quote2)
+print(quote3)
+print(quote4)
